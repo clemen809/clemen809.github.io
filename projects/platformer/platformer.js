c6f5f1d94f9e9999c185c35ec1f17415a43076ce
clemen809 <157505919+clemen809@users.noreply.github.com>
2024-02-08 16:23:01 +0000

    add platforms and collectables to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -43,8 +43,8 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
+    //createPlatform(500, 300, 200, 20); // short but wide platform located 500 pixels from the left of the screen and 300 pixels from the top of the screen
+    // createPlatform(500 100, 400, 10);
 
     
     // TODO 2
